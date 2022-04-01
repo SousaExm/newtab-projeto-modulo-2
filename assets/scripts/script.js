b7dfb891dc0a3c70e20a2f1e115febf09832f613
Renato Sousa <94645489+SousaExm@users.noreply.github.com>
2022-03-31 22:29:13 -0300

    adicionando o modal, finalizando o css e responsividade

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,34 @@
 let navMenu = document.getElementById('nav-menu')
+let menu = document.getElementById('menu')
+let close = document.getElementById('close')
 
-function changeMenu(){
+function changeMenu() {
     if(window.innerWidth > 768){
         navMenu.innerHTML = `<button>Cadastro de transações</button>
         <p class="line"></p>
         <button>Limpar dados</button>`
     }else{
-        navMenu.innerHTML =  `<div class="hamburger-lines">
+        navMenu.innerHTML =  `<div id="open-menu" class="hamburger-lines">
         <span class="line line1"></span>
         <span class="line line2"></span>
         <span class="line line3"></span>
-        </div>` 
+        </div>`
+        
+        let menuOn = document.getElementById('open-menu')
+        menuOn.addEventListener('click', () =>{
+            menu.classList.add('on')
+        })
+
     }
 }
 
+
+
 changeMenu()
 
+close.addEventListener('click', () =>{
+    menu.classList.remove('on')
+})
 window.addEventListener('resize', () =>{
    changeMenu()
 })
